ad2352b070c5f40f53212b15c9233e5549d20304
rpaternina <robertpaternina65@gmail.com>
2024-10-21 18:38:46 -0500

    Usando GetMaping para mapear direcciones

---
+++ src/main/java/cursoSpringBoot/controllers/HelloWorldRestController.java
@@ -0,0 +1,16 @@
+package cursoSpringBoot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        System.out.println("Ejecutandose");
+        return "Hello World";
+    }
+    
+
+}
